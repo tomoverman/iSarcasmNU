625f35d1f28a97a0f936b694e8e916391acf95c7
Addison Howe <addi.howe@gmail.com>
2021-12-03 16:14:52 -0600

    deleted cnn example scripts

--- example_runs/plot_cnn.sh
+++
@@ -1,7 +0,0 @@
-# Plot the results of training a CNN model.
-# Run with the command
-#      sh example_runs/plot_cnn.sh <path_to_training_results>
-# Loads and plots the data in the given text file.
-# Generates a plot of this data in the directory out/cnn_test_results
-
-python main.py cnn plot_training_results --training_results $1 --outdir out/cnn_test_results
--- example_runs/train_cnn.sh
+++
@@ -1,9 +0,0 @@
-# Train a CNN model over a specified number of epochs.
-# Run with the command
-#      sh example_runs/train_cnn.sh <n>
-# where n is the desired number of training epochs.
-# Saves the model in the file pretrained/cnn_<n>.pth
-# Saves training and testing data (losses and accuracies) in the directory out/cnn
-# Also generates a plot of this data in the directory out/cnn
-
-python main.py cnn train --num_epochs $1 --save_model pretrained/cnn_$1.pth --outdir out/cnn
