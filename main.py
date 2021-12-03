143bdf73b5f677ede6307be4d7569c1f48269dd5
Addison Howe <addi.howe@gmail.com>
2021-12-02 23:56:49 -0600

    forgot to push changes to main.py

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 
 import argparse
+import os
 from preprocessing.preprocessor import Preprocessor
-from models.cnn import CNN, CNNLayered
-from models.LSTMSarcasm import LSTMSarcasm
-from models.LSTM_Att import LSTM_Att
+from utils import select_model, train, test, plot_training_data, \
+                  save_training_results, save_testing_results
 import torch
 import torch.nn as nn
 import torch.optim as optim
@@ -12,164 +12,99 @@ from torch.utils.data import DataLoader
 
 def main():
 
-    parser = argparse.ArgumentParser(description='Model Training and Testing')
-    parser.add_argument("model", type=str, choices=["cnn", "3cnn", "lstm", "lstm_att", "siarn", "miarn"])
-    parser.add_argument("--data_train_fpath",   type=str,       default="./data/ptacek_data_train.csv")
-    parser.add_argument("--data_test_fpath",    type=str,       default="./data/ptacek_data_test.csv")
-    parser.add_argument("--batch_size",         type=int,       default=512)
-    parser.add_argument("--num_epochs",         type=int,       default=30)
-    parser.add_argument("--seq_len",            type=int,       default=40)
-    parser.add_argument("--min_len",            type=int,       default=5)
-    parser.add_argument("--embed_size",         type=int,       default=100)
-    parser.add_argument("--learning_rate",      type=float,     default=0.001)
-    parser.add_argument("--regularization",     type=float,     default=1e-8)
-    parser.add_argument("--clip",               type=int,       default=5)
+    parser = argparse.ArgumentParser(description='iSarcasmNU')
+
+    parser.add_argument("model",    type=str, choices=["cnn", "3cnn", "lstm", "lstm_att", "siarn", "miarn"])
+    parser.add_argument("action",   type=str, choices=["train", "test", "plot_training_results"])
+    parser.add_argument("--data_train_fpath",   type=str,       default="./data/ptacek_data_train.csv",
+                        help="path to training data")
+    parser.add_argument("--data_test_fpath",    type=str,       default="./data/ptacek_data_test.csv",
+                        help="path to testing data")
+    parser.add_argument("--batch_size",         type=int,       default=512,
+                        help="testing and training batch size")
+    parser.add_argument("--num_epochs",         type=int,       default=30,
+                        help="number of training epochs")
+    parser.add_argument("--seq_len",            type=int,       default=40,
+                        help="fixed length of input (number of tokens in tweet)")
+    parser.add_argument("--min_len",            type=int,       default=5,
+                        help="minimum number of tokens in input sequence")
+    parser.add_argument("--embedding_size",     type=int,       default=100,
+                        help="dimension of the embedding space")
+    parser.add_argument("--learning_rate",      type=float,     default=0.001,
+                        help="learning rate hyperparameter")
+    parser.add_argument("--regularization",     type=float,     default=1e-8,
+                        help="L2 regularization hyperparameter")
+    parser.add_argument("--clip",               type=int,       default=5,
+                        help="clipping amount to prevent exploding gradients")
+    parser.add_argument("--save_model",         type=str,       default="",
+                        help="file path to save model parameters to)")
+    parser.add_argument("--load_model",         type=str,       default="",
+                        help="path to saved model file")
+    parser.add_argument("--outdir",             type=str,       default="out",
+                        help="path to directory to save output")
+    parser.add_argument("--training_results",   type=str,       default="",
+                        help="path to file containing training results")
+
     args = parser.parse_args()
 
     model_name          = args.model
+    action              = args.action
     data_train_fpath    = args.data_train_fpath
     data_test_fpath     = args.data_test_fpath
     batch_size          = args.batch_size
     num_epochs          = args.num_epochs 
     seq_len             = args.seq_len 
     min_len             = args.min_len 
-    embed_size          = args.embed_size 
+    embed_size          = args.embedding_size 
     learning_rate       = args.learning_rate 
     reg_l2              = args.regularization 
     clip                = args.clip
-    
+    save_model_path     = args.save_model
+    load_path           = args.load_model
+    outdir              = args.outdir
+    train_results_path  = args.training_results
 
+    # Make output directory if needed
+    os.makedirs(outdir, exist_ok=True)
+    
     # Preprocess data
     prep = Preprocessor(seq_len=seq_len, min_len=min_len)
     prep.load_data(data_train_fpath, data_test_fpath)
     prep.initialize()
     vocab_size  = prep.V
 
-    # Construct model
-    if model_name.lower() == "cnn":
-        # Parameters for CNN
-        filter_size = 3
-        num_filters = 100
-        model = CNN(num_filters, filter_size, embed_size, vocab_size, seq_len)
-
-    elif model_name.lower() == "3cnn":
-        # Parameters for 3CNN
-        filter_sizes    = [3, 4, 5]
-        filter_counts   = [100, 100, 100]
-        model = CNNLayered(filter_counts, filter_sizes, embed_size, vocab_size, seq_len)
-
-    elif model_name.lower() == "lstm":
-    	# Parameters for LSTM
-    	hidden_dim = 100
-    	model = LSTMSarcasm(embed_size, hidden_dim, vocab_size)
-
-    elif model_name.lower() == "lstm_att":
-    	# Parameters for LSTM_ATT
-        hidden_dim = 100
-        model = LSTM_Att(embed_size, hidden_dim, vocab_size, seq_len)
-
-    elif model_name.lower() == "siarn":
-        hidden_dim = 100
-        model = SIARN(embed_size, hidden_dim, vocab_size, seq_len)
-
-    elif model_name.lower() == 'miarn':
-        # Parameters for MIARN
-        pass
+    # Select model
+    model = select_model(model_name, embed_size, vocab_size, seq_len, load_path)
     
     # Specify optimizer and loss function
     optimizer = optim.RMSprop(model.parameters(), lr=learning_rate, weight_decay=reg_l2)
     criterion = nn.BCELoss()
     
-    # Load datasets
-    train_loader = DataLoader(dataset=prep.get_dataset_train(), batch_size=batch_size, shuffle=True)
-    test_loader  = DataLoader(dataset=prep.get_dataset_test(),  batch_size=batch_size, shuffle=False)
+    # Actions
+    if action == "train":
+        # Load data, then train and test the model
+        train_loader = DataLoader(dataset=prep.get_dataset_train(), batch_size=batch_size, shuffle=True)
+        test_loader  = DataLoader(dataset=prep.get_dataset_test(),  batch_size=batch_size, shuffle=False)
+        train_losses, accuracies = train(model, num_epochs, train_loader, optimizer, criterion, clip)
+        precision, recall, accuracy, fscore = test(model, test_loader)
 
-    # Train model    
-    train_losses, test_losses, accuracies = train(model, num_epochs, train_loader, optimizer, criterion, clip)
+        # Save model if desired
+        if save_model_path:
+            torch.save(model.state_dict(), save_model_path)
+        
+        # Save and plot results
+        save_training_results(model_name, train_losses, accuracies, outdir)
+        plot_training_data(model_name, train_losses, accuracies, outdir)
+        save_testing_results(model_name, precision, recall, accuracy, fscore, outdir)
 
-    # Test model
-    precision, recall, accuracy, fscore = test(model, test_loader)
-    
+    elif action == "test":
+        # Load test data, test the model, and save the results
+        test_loader  = DataLoader(dataset=prep.get_dataset_test(),  batch_size=batch_size, shuffle=False)
+        precision, recall, accuracy, fscore = test(model, test_loader)
+        save_testing_results(model_name, precision, recall, accuracy, fscore, outdir)
 
-def train(model, num_epochs, train_loader, optimizer, loss_function, clip):
-    train_losses = []
-    test_losses  = []
-    accuracies   = []
-    
-    model.train()
-    for epoch in range(1, num_epochs + 1):
-        for i, (xs, labels) in enumerate(train_loader):
-
-            # Perform forward pass
-            outputs = model(xs)
-            pred_labels = convert_output_to_label(outputs)
-
-            # Compute and store loss and accuracy
-            loss = loss_function(outputs, labels.float())
-            accuracy = get_accuracy(pred_labels, labels)
-            train_losses.append(loss.item())
-            accuracies.append(accuracy)
-
-            # Backprop
-            optimizer.zero_grad()
-            loss.backward()
-            nn.utils.clip_grad_norm_(model.parameters(), clip)
-            optimizer.step()
-
-        print(f'Epoch [{epoch}/{num_epochs}], Loss: {loss:.4f}, Accuracy: {accuracy:.2f}')
-
-    return train_losses, test_losses, accuracies
-
-
-def test(model, test_loader):
-    model.eval()
-    with torch.no_grad():
-        correct = 0
-        total = 0
-        false_positives = 0
-        false_negatives = 0
-        true_positives  = 0
-        for xs, labels in test_loader:
-            outputs = model(xs)
-            pred_labels = convert_output_to_label(outputs)
-            total += len(labels)
-            correct += (pred_labels == labels).sum().int()
-            false_negatives += get_false_negatives(pred_labels, labels)
-            false_positives += get_false_positives(pred_labels, labels)
-            true_positives  += get_true_positives(pred_labels, labels)
-
-    # Calculate precision, recall, accuracy, and F-score
-    precision = true_positives / (true_positives + false_positives)
-    recall = true_positives / (true_positives + false_negatives)
-    accuracy = correct / total
-    fscore = 2. * (precision * recall) / (precision + recall)
-
-    print(f"Test Precision: {precision}")
-    print(f"Test Recall: {recall}")
-    print(f"Test Accuracy: {accuracy}")
-    print(f"Test F-score: {fscore}")
-
-    return precision, recall, accuracy, fscore
-
-
-########################
-##  Helper functions  ##
-########################
-
-def convert_output_to_label(cnn_output):
-    return torch.round(cnn_output).int()
-
-def get_accuracy(y_pred, y_true):
-    return (y_pred == y_true).sum().float() / y_true.shape[0]
-
-def get_false_positives(y_pred, y_true):
-    return torch.logical_and(y_pred != y_true, y_pred == 1).sum().int()
-
-def get_false_negatives(y_pred, y_true):
-    return torch.logical_and(y_pred != y_true, y_pred == 0).sum().int()
-
-def get_true_positives(y_pred, y_true):
-    return torch.logical_and(y_pred == y_true, y_pred == 1).sum().int()
+    elif action == "plot_training_results":
+        plot_training_data(model_name, None, None, outdir, train_results_path)
 
 
 if __name__ == "__main__":
